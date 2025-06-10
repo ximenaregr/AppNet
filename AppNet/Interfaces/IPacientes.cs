78b5ea7fdb8815aa2787aabf635b92b29721b739
ximenare <regisyxime@gmail.com>
2025-06-09 19:40:14 -0600

    act2

--- AppNet/Interfaces/IPacientes.cs
+++ AppNet/Interfaces/IPacientes.cs
@@ -6,9 +6,9 @@ public interface IPacientes
 {
     public Task<List<Paciente2>> GetAll();
     public Task<Paciente2> GetById(int id);
-    public Task<int> InsertPaciente(Paciente2 A);
-    public Task<int> DeletePaciente(Paciente2 A);
-    public Task<int> UpdatePaciente(Paciente2 A);
+    public Task<int> InsertPaciente(Paciente2 paciente);
+    public Task<int> DeletePaciente(Paciente2 paciente);
+    public Task<int> UpdatePaciente(Paciente2 paciente);
 
 }
 
